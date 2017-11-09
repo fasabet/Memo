2d296b74fd2890d37b648b5e066a05d2456030c6
fasabet@gmail.com <fasabet@gmail.com>
2017-11-09 21:07:33 +0100

    black

--- memo.php
+++ memo.php
@@ -10,10 +10,9 @@
                     width : 50px;
                     height: 50px;
                     cursor: crosshair;
+                    background-color: black;
                  }
-                 .button:hover{
-                     background-color :lightpink;
-                 }
+                 
                 
                 
             </style>
@@ -25,20 +24,22 @@
     <body>
         <table border="1">
             <?php
+                $a= 0;
             for ($row = 1; $row <= 4; $row++) {
                 echo "<tr>";
                 for ($colon = 1; $colon <= 4; $colon++) {
                     $cell = "$row" . "$colon";
-                   $platje=["A","B","C","D","W","X","Y","Z"];
-             for ($a=0;$a< count$platje;$a++){
-                 
+                $platje=["A","B","C","D","W","X","Y","Z","A","B","C","D","W","X","Y","Z"];                          
               
-             echo "<td $cell class=button></td>";
-                    // echo "<td onclick=shoot($row$colon) id=board$row$colon class=button ></td>";
-                }
-                echo"</tr>";
+         
+                echo "<td onclick=draai($row$colon) id=board$row$colon class=button >$platje[$a]</td>";
+             $a++;
             }
+             echo"</tr>";
+            
             }
+            
+            
             /*    
             $gussy=(rand(0,3));
             $gussx=(rand(0,3));  
@@ -49,7 +50,18 @@
             
             ?>
         </table>
-        
+        <script>
+          var piet;
+            function draai(q){
+               document.getElementById("board"+q).style.background='yellow';
+               piet = q;
+               setTimeout (jojo,3000);
+           }
+            function jojo(){
+               
+               document.getElementById("board"+piet).style.background='black';
+            }
+        </script>
         
         
         
